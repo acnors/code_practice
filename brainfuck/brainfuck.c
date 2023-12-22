2f9c9f616d85ea5215825df4785588d4af9c52dc
acnors <83560816+acnors@users.noreply.github.com>
2023-12-22 12:30:11 -0500

    Update brainfuck.c
    .

--- brainfuck/brainfuck.c
+++ brainfuck/brainfuck.c
@@ -1,16 +1,30 @@
 #include <stdio.h>
 
-int tape[];
-int position = 0;
+#define TAPE_SIZE 100
 
-int add(position){
-tape[position]++;
+int tape[TAPE_SIZE];
+int position;
+
+int add(int position) {
+    tape[position]++;
+    return position;
 }
 
-int subtract(position){
-tape[position]--;
+int subtract(int position) {
+    tape[position]--;
+    return position;
+}
+
+int move_position(int position) {
+    return position + 1;
 }
 
 int main() {
+    position = move_position(position);
+	tape[position] = add(position);
+    printf("Position: %d\n", position);
+    printf("Tape[0]: %d\n", tape[position]);
 
+    return 0;
 }
+
